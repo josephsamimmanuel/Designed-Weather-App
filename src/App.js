8a7d38b0f5da9b43340dfc3fddad7b761dfaa8be
josephsamimmanuel <josesamimmanuel@gmail.com>
2024-03-06 22:50:01 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ setvalue(event.target.value)
   return (
       <div className="flex flex-row justify-center h-[100vh] items-center" >
           <div style={{backgroundImage: "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)"}} className=" p-5 rounded-md shadow">
-              <h2 className=" font-bold text-4xl m-5 mb-4">Hey!🌤️</h2>
-              <p  className=" text-2xl m-5">Do you want to know the weather Report :)</p>
+              <h1 className=" font-bold text-4xl m-5 mb-4">Hey!🌤️</h1>
+              <p  className=" text-xl m-5">Do you want to know the weather Report :)</p>
               <input onChange={handleinput} type="text" className=" rounded-md h-6 w-80 text-base mb-3 ml-5 p-5 mt-2 outline-none" placeholder="City name" /><br />
               <button onClick={getdata} className=" bg-black text-white rounded-lg p-2 text-lg mb-3 ml-5 mt-4">Get Report⚡</button>
              <p className=" text-lg mt-2 p-5"> <i class="fa-solid fa-temperature-quarter"></i> Degree: {deg} | <i class="fa-solid fa-location-dot"></i>  City: {city} | <i class="fa-solid fa-cloud-rain"></i> Weather: {desc}</p>
